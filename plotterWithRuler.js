5ec8e5e640b3148a54e7fb329e21e6822859a592
Abigail Fernandes <abigailferns94@gmail.com>
2018-03-14 12:11:03 +0530

    Add Bootstrap and refactor code

--- plotterWithRuler.js
+++ plotterWithRuler.js
@@ -1,131 +1,79 @@
 // Variable initialization
-var canvas = document.getElementById("canvasTop");
-var ctx = canvas.getContext("2d");
-var canvas2 = document.getElementById("canvasBottom");
-var ctx2 = canvas2.getContext("2d");
-var canvasOffset = $("#canvasTop").offset();
-var offsetX = canvasOffset.left;
-var offsetY = canvasOffset.top;
-var functElement = document.getElementById("function");
-var gridElement = document.getElementById("gridSpace");
+const canvas = document.getElementById("canvasTop");
+const ctx = canvas.getContext("2d");
+const canvas2 = document.getElementById("canvasBottom");
+const ctx2 = canvas2.getContext("2d");
+const canvasOffset = $("#canvasTop").offset();
+const offsetX = canvasOffset.left;
+const offsetY = canvasOffset.top;
+const functElement = document.getElementById("function");
+const gridElement = document.getElementById("gridSpace");
 
 // Function to display the position of the cursor
-function handleMouseMove(e) {
+const handleMouseMove = (e) => {
     mouseX = parseInt(e.clientX - offsetX);
-    mouseY = 1 - (2*(e.clientY - offsetY)/canvas.height);
-    $("#movelog").html("Coursor points: x = " + mouseX + " , y = " + mouseY.toFixed(3));
+    mouseY = 1 - (2 * (e.clientY - offsetY) / canvas.height);
+    $("#movelog").html("Cursor co-ordinates: x = " + mouseX + " , y = " + mouseY.toFixed(3));
 }
 
-$("#canvasTop").mousemove(function (e) {
-    handleMouseMove(e);
-});
+$("#canvasTop").mousemove(e => handleMouseMove(e));
 
-function init() {
-    var gridSpace = gridElement.options[gridElement.selectedIndex].value;
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx2.clearRect(0,0,canvas2.width,canvas2.height);
+const drawCanvas = () => {
+    const gridSpace = gridElement.options[gridElement.selectedIndex].value;
+    const typeOfWave = functElement.options[functElement.selectedIndex].value;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
     drawRulerGrid(gridSpace);
-    
     ctx.beginPath();
-    drawSine(ctx,canvas.width,canvas.height,gridSpace);  
+    drawGraph(ctx, canvas.width, canvas.height, gridSpace, typeOfWave);
     ctx.stroke();
 }
 
-function changeGridSpace() {   
-    var gridSpace = gridElement.options[gridElement.selectedIndex].value;
-    var funct = functElement.options[functElement.selectedIndex].value;
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx2.clearRect(0,0,canvas2.width,canvas2.height);
-    drawRulerGrid(gridSpace);
-    
-    ctx.beginPath();
-    if(funct==0) {
-        drawSine(ctx,canvas.width,canvas.height,gridSpace);   
-    }
-    else{
-        drawCosine(ctx,canvas.width,canvas.height,gridSpace) ;  
-    }
-    ctx.stroke();
-}
-
-function plotFunction() {
-    var gridSpace = gridElement.options[gridElement.selectedIndex].value;
-    var funct = functElement.options[functElement.selectedIndex].value;
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx2.clearRect(0,0,canvas2.width,canvas2.height);
-    drawRulerGrid(gridSpace, funct);
-    
-    ctx.beginPath();
-    if(funct==0) {
-        drawSine(ctx,canvas.width,canvas.height,gridSpace);   
-    }
-    else{
-        drawCosine(ctx,canvas.width,canvas.height,gridSpace);  
-    }
-    ctx.stroke();
-}
-
-function drawRulerGrid(gridSpace, funct){
-    var x,y;
+const drawRulerGrid = (gridSpace) => {
     ctx.beginPath();
     ctx2.beginPath();
-    // gridSpace = 50;
-    ctx.strokeStyle = '#000';
-    ctx.fillStyle = '#fff'
-    
-    for (var i = 0; i < canvas.width; i += 10) {
-        if(i/gridSpace == parseInt(i/gridSpace)){
+    ctx.strokeStyle = '#dadae2';
+    ctx.fillStyle = '#dadae2'
+
+    for (let i = 0; i < canvas.width; i += 10) {
+        let x, y;
+        if (i / gridSpace == parseInt(i / gridSpace)) {
             y = 0;
-            ctx.moveTo(i , 0);
-            ctx.lineTo(i ,canvas.height);
-            ctx2.strokeText(i/gridSpace, i+15, 15);
-        }
-        else {
+            ctx.moveTo(i, 0);
+            ctx.lineTo(i, canvas.height);
+            ctx2.strokeText(i / gridSpace, i + 15, 15);
+        } else {
             y = 10
         }
         ctx2.moveTo(i + 25, y);
         ctx2.lineTo(i + 25, 15);
 
-        if(i / gridSpace == parseInt(i / gridSpace)) {
+        if (i / gridSpace == parseInt(i / gridSpace)) {
             x = 5;
-            ctx.moveTo(0 , i);
-            ctx.lineTo(canvas.width ,i);
-            ctx2.strokeText((canvas.height - 2*i)/canvas.height, 0, i+15);
-        }
-        else {
+            ctx.moveTo(0, i);
+            ctx.lineTo(canvas.width, i);
+            ctx2.strokeText((canvas.height - 2 * i) / canvas.height, 0, i + 15);
+        } else {
             x = 15;
         }
-        // var x = (i / 50 == parseInt(i / 50)) ? 0 : 10;
         ctx2.moveTo(x, i + 15);
         ctx2.lineTo(25, i + 15);
     }
     ctx.stroke();
-    ctx2.stroke(); 
+    ctx2.stroke();
 }
 
-
-function drawSine(context, width, height, gridSpace) {
-
-    context.strokeStyle = '#00f';
-    context.fillStyle = '#fff';
+const drawGraph = (context, width, height, gridSpace, typeOfWave) => {
+    context.strokeStyle = '#0D47A1';
+    context.fillStyle = '#0D47A1';
     context.moveTo(0, 250);
-    
-    for (i = 0; i <= width; i += 10) {
-        x = i*(Math.PI/(gridSpace*2));
-        y = Math.sin(-x);
-        context.lineTo(i, parseInt(y*250 + 250));
+    const waveFunctions = {
+        sine: (y) => Math.sin(-y),
+        cosine: (y) => -Math.cos(y),
     }
-}
-
-function drawCosine(context, width, height, gridSpace) {
-
-    context.strokeStyle = '#00f';
-    context.fillStyle = '#fff';
-    context.moveTo(0, 250);
-    
-    for (i = 0; i <= width; i += 10) {
-        x = i*(Math.PI/(gridSpace*2));
-        y = -Math.cos(x);
-        context.lineTo(i, parseInt(y*250 + 250));
+    for (let i = 0; i <= width; i += 10) {
+        x = i * (Math.PI / (gridSpace * 2));
+        y = waveFunctions[typeOfWave](x);
+        context.lineTo(i, parseInt(y * 250 + 250));
     }
 }
